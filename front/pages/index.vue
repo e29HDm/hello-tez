2c472220d8c0cdfbe61e168d53e2321f1c414c6a
e29HDm <delignekylian@gmail.com>
2023-12-19 02:28:59 +0100

    Add useWallet hook to index.vue

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
-import 
+import { useWallet } from "../services/Wallet";
+
+const wallet = await useWallet();
+wallet.tezos?.contract
+  .at("KT1QtDGAu4gTgr4DfJGAzrwrn6txXFuXJ2Ha")
+  .then((c) => {
+    let methods = c.parameterSchema.ExtractSignatures();
+    console.log(JSON.stringify(methods, null, 2));
+  })
+  .catch((error) => console.log(`Error: ${error}`));
 </script>
 
 <template>
